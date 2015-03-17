6dc2ecb0a1a09acd92e1c243d5fa59bfb0d3aeb2
ykuo <alice.ykuo@gmail.com>
2015-03-17 15:49:11 -0700

    updated doc string for extract_patches.py

--- code/extract_patches.py
+++ code/extract_patches.py
@@ -1,9 +1,5 @@
-import scipy
-from PIL import Image
-from image_loader import ImageLoader
 import numpy as np
 import pickle as pkl
-from helper import star
 from dom_colors import DominantColor
 from sklearn.feature_extraction import image
 from skimage import io
@@ -11,6 +7,11 @@ from sklearn.preprocessing import StandardScaler
 
 
 class ExtractPatches(object):
+    '''
+    DOC:
+    ExtractPatches converts an image into smaller patches determined by
+    each patch's size and the number of patches desired.
+    '''
 
     def __init__(self, img_num, root_dir='/Users/kuoyen/Documents/myweddingstyle/images/uniform_100'):
         dc = DominantColor(img_num, root_dir)
@@ -19,8 +20,7 @@ class ExtractPatches(object):
         self.all_img_mat = dc.all_img_mat[0]
         self.label_vector = dc.label_vector
 
-
-    def extract_patch(self, img, patch_size = (20,20), max_patches = 25, dominant_color_vector = None):
+    def extract_patch(self, img, patch_size=(20, 20), max_patches=25, dominant_color_vector=None):
         img = io.imread(img)
         if dominant_color_vector is None:
             raise 'No dominant colors found!'
@@ -28,42 +28,23 @@ class ExtractPatches(object):
         slices = max_patches
         arr = []
         for i in xrange(slices):
-            # print  "patches[i].ravel() shape", patches[i]
-            # print "completed print patches"
             patch = np.concatenate(patches[i].ravel()[np.newaxis, :], axis=1)
-            dom_col_patch = np.concatenate((dominant_color_vector, patch), axis=1 )
-            # print "printing patch type ", type(patch)
-            # print "$$$$$$", type(dominant_color_vector)
+            dom_col_patch = np.concatenate((dominant_color_vector, patch), axis=1)
             arr.append(dom_col_patch)
         arr = np.array(arr)
-        print "@@@@@@@@@@@", arr
-        print np.array(arr).shape
-        #     arr.append(np.concatenate(arr[np.newaxis, :], axis=1))
-        # # arr = np.array(arr)
-        # # print arr.shape
         return arr
 
     def run_extract_patch(self):
-        print "********* number of files processing", len(self.all_files)
-        print "all colors mat shape", self.all_colors_mat.shape
-        self.label_vector = np.array(self.label_vector)[:,np.newaxis]
-        label_color_mat = np.concatenate((self.label_vector, self.all_colors_mat), axis = 1)
-        # print "@@@", label_color_mat
+        print "Number of files processing", len(self.all_files)
+        self.label_vector = np.array(self.label_vector)[:, np.newaxis]
+        label_color_mat = np.concatenate((self.label_vector, self.all_colors_mat), axis=1)
         all_img_patches = []
         for img, dom_color_vec in zip(self.all_files, label_color_mat):
-            print "________________________________________________"
-            img_patches = self.extract_patch(img = img, dominant_color_vector = dom_color_vec)
-            print "@@@@@", img_patches.shape
+            img_patches = self.extract_patch(img=img, dominant_color_vector=dom_color_vec)
             all_img_patches.append(np.array(img_patches))
 
-        # feat_matrix = np.append((np.array(all_img_patches)), axis =0)
-        #     if feat_matrix is None:
-        #         feat_matrix = img_patches
-        #     else:
-        #         feat_matrix = np.concatenate((feat_matrix, img_patches), axis =0)
-        # print feat_matrix.shape
         self.feature_mat = np.concatenate(all_img_patches, axis=0)
-        print "DONE with FEATURE MATRIX:", self.feature_mat.shape
+        print "COMPLETED FEATURE MATRIX:", self.feature_mat.shape
         return self.feature_mat
 
     def standard_scaler(self):
@@ -77,12 +58,8 @@ class ExtractPatches(object):
         pkl.dump(self.feature_mat_scaled, open(pkl_path, 'wb'))
         print "finished pickling", pkl_path
 
-    # self.y = np.array(self.label_vector)[:,np.newaxis]
-
 if __name__ == '__main__':
     ep = ExtractPatches(1000)
     ep.run_extract_patch()
-    ep.standard_scaler()
+    # ep.standard_scaler()
     ep.save_pkl()
-
-
