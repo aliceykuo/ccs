6b9e6d3a66e7e7029f7de85e622e3ec5c00c1dac
ykuo <alice.ykuo@gmail.com>
2015-03-17 16:53:47 -0700

    cleaned up helper.py

--- code/helper.py
+++ code/helper.py
@@ -1,8 +1,11 @@
-__author__ = 'kuoyen'
-
 import os
 import errno
 from skimage import io
+
+'''
+DOC:
+This file contains general functions.
+'''
 
 
 def write_image(img_matrix, filename):
@@ -16,25 +19,23 @@ def mkdir_p(path):
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
 
+
+def pad_list(lst):
+    inner_max_len = max(map(len, lst))
+    map(lambda x: x.extend([0]*(inner_max_len-len(x))), lst)
+    return np.array(lst)
+
+
 def star(n):
     print ''.join(['-'] * n)
-# def filter_function(img, filter='hist'):
-#     if filter == 'hist':
-#         filtered_image = equalize_hist(img)
-#     elif filter == 'background':
-#         filtered_image = bkg()
-#     elif filter == 'segment':
-#         filtered_image = slic(img, n_segments=3, compactness=10, sigma=1)
-#     return filtered_image
-#
-# def whiten(img):
-#     mu = np.mean(img)
-#     std = np.std(img)
-#     img = (img - mu) / std
-#     return img
-#
-# #
-# def pad_list(lst):
-#     inner_max_len = max(map(len, lst))
-#     map(lambda x: x.extend([0]*(inner_max_len-len(x))), lst)
-#     return np.array(lst)
+
+
+def delete_files():
+    folder = '/Users/kuoyen/Documents/myweddingstyle/images/transformed_1'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception, e:
+            print e
